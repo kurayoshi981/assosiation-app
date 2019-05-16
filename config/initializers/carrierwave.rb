7af8bbf60d2cd690b9842bd5ee3b7ce0f66ca60d
kurayoshi981 <tachp981@yahoo.co.jp>
2019-05-16 14:21:58 +0900

    fix:画像が保存できないエラー修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,15 +2,17 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
- CarrierWave.configure do |config|
-    config.storage :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1'
-    }
-    config.fog_directory  = 'pastank981'
-    config.asset_host = 'https://s3.amazonaws.com/pastank981'
+if Rails.env.production?
+    CarrierWave.configure do |config|
+        config.storage :fog
+        config.fog_provider = 'fog/aws'
+        config.fog_credentials = {
+        provider: 'AWS',
+        aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+        region: 'ap-northeast-1'
+        }
+        config.fog_directory  = 'pastank981'
+        config.asset_host = 'https://s3.amazonaws.com/pastank981'
+    end
 end
